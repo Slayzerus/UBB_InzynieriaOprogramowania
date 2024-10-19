b4e38cd3dcfad3219b474bcf2e4b933edb7b82cf
Slayzer <slayzer@o2.pl>
2024-10-19 18:44:29 +0200

    Program + Controller

---
+++ NiceToDev.Schedules/NiceToDev.Schedules/Controllers/SchedulesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NiceToDev.Schedules.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NiceToDev.Schedules/NiceToDev.Schedules/Program.cs
+++ NiceToDev.Schedules/NiceToDev.Schedules/Program.cs
@@ -1,8 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using NiceToDev.Schedules.Application.Interfaces;
+using NiceToDev.Schedules.Application.Services;
+using NiceToDev.Schedules.Database;
+using NiceToDev.Schedules.Domain.Interfaces;
+using NiceToDev.Schedules.Interfaces.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ScheduleContext>(
+        options => options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection"),
+            b => b.MigrationsAssembly("NiceToDev.Schedules")));
+
+builder.Services.AddScoped<IScheduleRepository, ScheduleRepository>();
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
